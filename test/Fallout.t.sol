27f727f6a12a477f9d272b86a2c04d45267226cb
adarsh.m <adarsh.m@simformsolutions.com>
2023-03-20 02:11:53 +0530

    removed files and solved fallout challenge

--- test/Fallout.t.sol
+++ test/Fallout.t.sol
@@ -1,50 +1,30 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity >=0.6.0;
-pragma experimental ABIEncoderV2;
+<<<<<<< HEAD
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.6.0;
 
 import "./utils/BaseTest.sol";
-import "src/levels/Fallout.sol";
-import "src/levels/FalloutFactory.sol";
+import "../src/levels/Fallout.sol";
 
-contract TestFallout is BaseTest {
-    Fallout private level;
+contract TestFallout is Test{
+    Fallout level;
+    address owner = address(0x01);
+    address player = address(0x02);
 
-    constructor() public {
-        // SETUP LEVEL FACTORY
-        levelFactory = new FalloutFactory();
+    function setUp() public{
+        level = new Fallout();
     }
 
-    function setUp() public override {
-        // Call the BaseTest setUp() function that will also create testsing accounts
-        super.setUp();
-    }
-
-    function testRunLevel() public {
-        runLevel();
-    }
-
-    function setupLevel() internal override {
-        /** CODE YOUR SETUP HERE */
-
-        levelAddress = payable(this.createLevelInstance(true));
-        level = Fallout(levelAddress);
-    }
-
-    function exploitLevel() internal override {
-        /** CODE YOUR EXPLOIT HERE */
+    function testFal1out() public{
+        vm.startPrank(owner); 
+        level = new Fallout();
+        assertEq(level.owner(), address(0));
+        vm.stopPrank();
 
         vm.startPrank(player);
-
-        // Before Solidity 0.4.22 the only way to define a constructor for a contract was to define a function with the same name of the contract itself
-        // After that version they introduced a new `constructor` keyword to avoid this kind of mistake
-        // In this case the developer made the mistake to misstype the name of the constructor
-        // Contract name -> Fallout
-        // Constructor name -> Fal1out
-        // The result of this is that the contract was never initialized, the owner was the address(0)
-        // and we were able to call the `Fal1out` function that at this point is not a constructor (callable only once)
-        // but a "normal" function. This also mean that anyone can call multiple time this function switching the owner of the contract.
         level.Fal1out();
-
+        assertEq(level.owner(), player);
         vm.stopPrank();
+
     }
+
 }
